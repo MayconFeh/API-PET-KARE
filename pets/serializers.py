b9ef01ff64efa856d9d395804634e190cda2f5ad
MayconFeh <mfbm87@gmail.com>
2023-10-03 12:55:15 -0300

    feat:API-COMPLETE

---
+++ pets/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from groups.serializers import GroupSerializer
+from traits.serializers import TraitSerializer
+from .models import Pet, Sex
+
+
+class PetSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(max_length=50)
+    age = serializers.IntegerField()
+    weight = serializers.FloatField()
+    sex = serializers.ChoiceField(choices=Sex.choices, default=Sex.DEFAULT)
+    group = GroupSerializer()
+    traits = TraitSerializer(many=True)
+
+    def create(self, validated_data):
+        return Pet.objects.create(**validated_data)
+    ...
